31cbac3683dc7f933d7c0abbad98fcb37575ad28
evasyukov <egor.vasyukov1@gmail.com>
2026-01-24 11:44:32 +0700

    - установка пакетов - базовая настройка проекта

---
+++ src/Mozaika.jsx
@@ -0,0 +1,20 @@
+import styled from "styled-components"
+
+export default function Mozaika() {
+  return <AppColumn> 
+    
+  </AppColumn>
+}
+
+const AppColumn = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+
+  position: relative;
+
+  width: 1000px;
+  min-height: 100%;
+
+  margin: 0 auto;
+`
--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,12 @@
 import { createRoot } from "react-dom/client"
-import "./index.css"
-import App from "./App.jsx"
 
-createRoot(document.getElementById("root")).render(<App />)
+import "./index.css"
+import Mozaika from "./Mozaika.jsx"
+
+createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
+    <Provider>
+      <Mozaika />
+    </Provider>
+  </BrowserRouter>,
+)
