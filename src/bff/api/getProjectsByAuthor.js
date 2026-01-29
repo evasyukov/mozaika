d3a75cbed47d24204817469c6c5ff2c36b41674a
evasyukov <egor.vasyukov1@gmail.com>
2026-01-29 11:12:13 +0700

    - исправил баг с запросом проектов пользователя

--- src/bff/api/getProjectsByAuthor.js
+++ src/bff/api/getProjectsByAuthor.js
@@ -1,7 +1,9 @@
 export async function getProjectsByAuthor(authorId) {
   const response = await fetch(
-    `http://localhost:3005/projects?authorId=${authorId}`,
+    `http://localhost:3005/projects?author_id=${authorId}`,
   )
   const users = await response.json()
+  console.log(users)
+
   return users
 }
